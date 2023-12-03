73920664b0992a9f56b9a154d0bfb06677d7eb76
den <dennnisong@gmail.com>
2023-12-03 00:10:56 -0800

    feat: trimmed excess

---
+++ src/routes/SoundButton.svelte
@@ -0,0 +1,17 @@
+<script>
+	async function playAudio() {
+		const audio = new Audio();
+		audio.src="https://freesound.org/data/previews/536/536420_4921277-lq.mp3";
+		audio.play();
+	}
+</script>
+
+<div class="counter">
+	<button on:click={playAudio} aria-label="Decrease the counter by one">
+		cool button click here for something
+	</button>
+</div>
+
+<style>
+
+</style>
